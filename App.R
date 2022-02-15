dd75511c38421dd567f0c71af5e8eb596edeaaea
Bonaventure Muia <58984297+kisoibo@users.noreply.github.com>
2022-02-15 15:56:06 +0300

    add the sliderinput()

--- App.R
+++ App.R
@@ -9,14 +9,21 @@ UI<-shinyUI(
   fluidPage(
     titlePanel(title = "This is my first shiny application"),
     sidebarLayout(
-      sidebarPanel(
+      sidebarPanel(h3("Enter personal information"),
         textInput("name","Enter your name",""),
-        textInput("age","Enter your age","")
+        textInput("age","Enter your age",""),
+        radioButtons("gender","Choose your gender","Male",choices = c("Male","Female")),
+        sliderInput("height","Select your height",min = 0,max = 3,value = 2,step = 0.5),
+        submitButton("Submit")
         
       ),
-      mainPanel(h3("Personal information"),
+      mainPanel(h3("Display Personal Information"),
         textOutput("myname"),
-        textOutput("myage")
+        textOutput("myage"),
+        textOutput("mygender"),
+        textOutput("myheight")
+        
+        
       )
     )
     
@@ -24,9 +31,12 @@ UI<-shinyUI(
 )
 
 server<-function(input,output,session){
-  output$myname=renderText({input$name})
-  output$myage=renderText({input$age})
+  output$myname=renderText({paste("My name is:",input$name)})
+  output$myage=renderText({paste("Your age is:",input$age,"Years")})
+  output$mygender<-renderText({paste("Your gender is:",input$gender)})
+  output$myheight<-renderText({paste("My height is:",input$height,"M")})
   
 }
 
 shinyApp(UI,server)
+ 
